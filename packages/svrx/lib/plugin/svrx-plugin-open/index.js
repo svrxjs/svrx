fcc06b311e296de7b2a35a42ad0e712323abad19
leeluolee <87399126@163.com>
2019-06-24 10:56:48 +0800

    fix(open): open.default changeto local

--- packages/svrx/lib/plugin/svrx-plugin-open/index.js
+++ packages/svrx/lib/plugin/svrx-plugin-open/index.js
@@ -1,40 +1,42 @@
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
 module.exports = {
-  configSchema: {},
   hooks: {
     async onCreate({ events, config, logger }) {
-      events.on('ready', () => {
-        let open = config.get('open');
+      events.on("ready", () => {
+        let open = config.get("open");
 
         const URL_MAPING = {
-          external: config.get('urls.external'),
-          local: config.get('urls.local'),
+          external: config.get("urls.external"),
+          local: config.get("urls.local")
         };
 
         if (open === false) return;
 
-        if (open === true) open = 'external';
+        if (open === true) open = "local";
 
-        if (typeof open !== 'string') return;
+        if (typeof open !== "string") return;
 
-        const openUrl = open.replace(/^(external|local)\b/, capture => URL_MAPING[capture]);
+        const openUrl = open.replace(
+          /^(external|local)\b/,
+          capture => URL_MAPING[capture]
+        );
 
-        openBrowser(openUrl, (err) => {
+        openBrowser(openUrl, err => {
           if (err) return logger.error(err);
         });
       });
-    },
-  },
+    }
+  }
 };
 
 function openBrowser(target, callback) {
   const map = {
-    darwin: 'open',
-    win32: 'start ',
+    darwin: "open",
+    win32: "start "
   };
 
-  const opener = map[process.platform] || 'xdg-open';
+  const opener = map[process.platform] || "xdg-open";
 
   return exec(`${opener} ${target}`, callback);
 }
