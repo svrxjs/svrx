c7a6e5e4cc0d963156f34b011b38002d126b8da8
xuchaoying <itsxcy@gmail.com>
2019-06-24 15:22:37 +0800

    docs(cors): add doc to plugin cors

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# svrx
+# svrx -- server-x
+
 platform built for efficient front-end development
+
+## Options
+
+### cors
+
+`boolean`, `object`
+
+Enable\disable cross-origin resource sharing([CORS](https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS)).
+Cors is enabled by default. 
+
+svrx makes use of [koa2-cors](https://github.com/zadzbw/koa2-cors) package.
+Check out its [option documentation](https://github.com/zadzbw/koa2-cors#options) for more advanced usages.
+
+
--- packages/svrx/lib/config-list.js
+++ packages/svrx/lib/config-list.js
@@ -1,80 +1,80 @@
-const { GROUPS } = require("./constant");
+const { GROUPS } = require('./constant');
 
 module.exports = {
   root: {
-    type: "string",
+    type: 'string',
     default: process.cwd(),
-    description: "where to start svrx",
-    defaultHint: "default to the current working directory",
+    description: 'where to start svrx',
+    defaultHint: 'default to the current working directory',
     group: GROUPS.CORE,
-    ui: false
+    ui: false,
   },
   dir: {
-    type: "string",
+    type: 'string',
     default: process.cwd(),
     group: GROUPS.CORE,
     cli: false,
-    ui: false
+    ui: false,
   },
   svrx: {
-    alias: "v",
-    type: "string",
-    description: "the version of svrx you want to use",
-    defaultHint: "default to the latest version installed locally",
+    alias: 'v',
+    type: 'string',
+    description: 'the version of svrx you want to use',
+    defaultHint: 'default to the latest version installed locally',
     group: GROUPS.CORE,
-    ui: false
+    ui: false,
   },
   port: {
-    alias: "p",
-    type: "number",
+    alias: 'p',
+    type: 'number',
     default: 8000,
-    description: "the unique identifier for a product",
-    group: GROUPS.CORE
+    description: 'the unique identifier for a product',
+    group: GROUPS.CORE,
   },
   https: {
-    type: "boolean",
-    group: GROUPS.CORE
+    type: 'boolean',
+    group: GROUPS.CORE,
   },
   urls: {
-    type: "object",
+    type: 'object',
     properties: {
       style: {
-        type: "string",
-        default: "/svrx/svrx-client.css",
+        type: 'string',
+        default: '/svrx/svrx-client.css',
         group: GROUPS.CORE,
         cli: false,
-        ui: false
+        ui: false,
       },
       script: {
-        type: "string",
-        default: "/svrx/svrx-client.js",
+        type: 'string',
+        default: '/svrx/svrx-client.js',
         group: GROUPS.CORE,
         cli: false,
-        ui: false
+        ui: false,
       },
       external: {
-        type: "string",
+        type: 'string',
         group: GROUPS.CORE,
         cli: false,
-        ui: false
+        ui: false,
       },
       local: {
-        type: "string",
+        type: 'string',
         group: GROUPS.CORE,
         cli: false,
-        ui: false
+        ui: false,
       },
       ui: {
-        type: "string",
+        type: 'string',
         group: GROUPS.CORE,
         cli: false,
-        ui: false
-      }
-    }
+        ui: false,
+      },
+    },
   },
   middlewares: {
-    type: "array",
-    group: GROUPS.CORE
+    type: 'array',
+    group: GROUPS.CORE,
   },
 
   // built plugin configs
@@ -83,99 +83,99 @@ module.exports = {
     default: true,
     anyOf: [
       {
-        type: "boolean"
+        type: 'boolean',
       },
       {
-        type: "object",
+        type: 'object',
         properties: {
           base: {
-            type: "string",
-            description: "where to serve content from",
-            defaultHint: "default to the current working directory(root)"
+            type: 'string',
+            description: 'where to serve content from',
+            defaultHint: 'default to the current working directory(root)',
           },
           headers: {
-            type: "object",
-            description: "add headers to all responses"
+            type: 'object',
+            description: 'add headers to all responses',
           },
           serveIndex: {
-            type: "boolean"
+            type: 'boolean',
           },
           historyApiFallback: {
-            anyOf: [{ type: "boolean" }, { type: "object" }],
-            errorMessage: "should be boolean or object"
-          }
-        }
-      }
+            anyOf: [{ type: 'boolean' }, { type: 'object' }],
+            errorMessage: 'should be boolean or object',
+          },
+        },
+      },
     ],
-    errorMessage: "should be boolean or object"
+    errorMessage: 'should be boolean or object',
   },
 
   proxy: {
     group: GROUPS.COMMON,
     anyOf: [
       {
-        type: "boolean"
+        type: 'boolean',
       },
       {
-        type: "object"
+        type: 'object',
       },
       {
-        type: "array"
-      }
+        type: 'array',
+      },
     ],
-    errorMessage: "should be boolean, array or object"
+    errorMessage: 'should be boolean, array or object',
   },
 
   livereload: {
-    description: "enable auto live reload",
+    description: 'enable auto live reload',
     group: GROUPS.COMMON,
     default: true,
     anyOf: [
       {
-        type: "boolean"
+        type: 'boolean',
       },
       {
-        type: "object",
+        type: 'object',
         properties: {
           exclude: {
             anyOf: [
-              { type: "string" },
-              { type: "array", items: { type: "string" } }
+              { type: 'string' },
+              { type: 'array', items: { type: 'string' } },
             ],
-            errorMessage: "should be string or array of string"
-          }
-        }
-      }
+            errorMessage: 'should be string or array of string',
+          },
+        },
+      },
     ],
-    errorMessage: "should be boolean or object"
+    errorMessage: 'should be boolean or object',
   },
 
   cors: {
-    description: "Cross-Origin Resource Sharing(CORS)",
+    description: 'Cross-Origin Resource Sharing(CORS)',
     group: GROUPS.COMMON,
     default: true,
     anyOf: [
       {
-        type: "boolean"
+        type: 'boolean',
       },
       {
-        type: "object"
-      }
+        type: 'object',
+      },
     ],
-    errorMessage: "should be boolean or object"
+    errorMessage: 'should be boolean or object',
   },
   open: {
-    description: "open something after server starting",
+    description: 'open something after server starting',
     group: GROUPS.COMMON,
-    default: "local",
+    default: 'local',
     anyOf: [
       {
-        type: "boolean"
+        type: 'boolean',
       },
       {
-        type: "string"
-      }
+        type: 'string',
+      },
     ],
-    errorMessage: "should be boolean or string"
-  }
+    errorMessage: 'should be boolean or string',
+  },
 };
