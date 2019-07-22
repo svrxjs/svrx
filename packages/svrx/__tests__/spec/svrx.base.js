2c9051b01a07151ef7775cc75c99d2db3aafdc8f
ZhengHaibo <87399126@163.com>
2019-07-22 19:55:56 +0800

    feat(core): addPublic api (#16)
    * feat(core): add public api
    
    * feat(core): public api support
    
    * test(base): fix svrx not required

--- packages/svrx/__tests__/spec/svrx.base.js
+++ packages/svrx/__tests__/spec/svrx.base.js
@@ -1,9 +1,13 @@
 const ffp = require('find-free-port');
 const request = require('supertest');
 const expect = require('expect.js');
+const sinon = require('sinon');
 const Koa = require('koa');
+const svrx = require('../../index');
+
 
 const Middleware = require('../../lib/middleware');
+
 const { createServer } = require('../util');
 
 const getPort = number => new Promise((resolve) => {
@@ -23,33 +27,33 @@ describe('Basic', () => {
 
   it('#start', (done) => {
     ffp(3000, '127.0.0.1', (err, p) => {
-      const svrx = createServer({
+      const svr = createServer({
         port: p,
         open: false,
       });
-      svrx.start((port) => {
+      svr.start((port) => {
         expect(port).to.eql(p);
-        svrx.close(done);
+        svr.close(done);
       });
     });
   });
 
   it('#start with no port', (done) => {
-    const svrx = createServer({
+    const svr = createServer({
       open: false,
     });
-    svrx.start((port) => {
+    svr.start((port) => {
       expect(port).to.not.equal(undefined);
-      svrx.close(done);
+      svr.close(done);
     });
   });
 
   it('#port conflict', (done) => {
     getPort().then((ps) => {
       const [p] = ps;
-      const svrx = createServer({ port: p, open: false });
+      const svr = createServer({ port: p, open: false });
 
-      svrx.start((port) => {
+      svr.start((port) => {
         expect(port).to.eql(p);
         const svrx2 = createServer({
           port: p,
@@ -58,7 +62,7 @@ describe('Basic', () => {
 
         svrx2.start((port2) => {
           expect(port2).to.not.equal(p);
-          svrx.close(() => svrx2.close(done));
+          svr.close(() => svrx2.close(done));
         });
       });
     });
@@ -99,7 +103,94 @@ describe('Middleware', () => {
 });
 
 describe('Public API', () => {
-  it('svrx.create() & svrx()', () => {
+  it('svrx.create() & svrx()', async () => {
+    const server = svrx({
+      open: false,
+      livereload: false,
+    });
 
+    const server2 = svrx.create({
+      open: false,
+      livereload: false,
+      plugins: [{
+        inplace: true,
+        name: 'hello-body',
+        hooks: {
+          async onRoute(ctx) {
+            ctx.body = 'hello';
+          },
+        },
+      }],
+    });
+    await server2.__svrx.setup();
+
+    await request(server.__svrx.callback())
+      .get('/')
+      .expect(404);
+
+    return request(server2.__svrx.callback())
+      .get('/')
+      .expect(200)
+      .expect('hello');
+  });
+
+  it('svrx.start svrx.close', async () => {
+    const TEST_PORT = 9002;
+
+    const server = svrx({
+      port: TEST_PORT,
+      open: false,
+      livereload: false,
+      plugins: [{
+        inplace: true,
+        name: 'hello-body',
+        hooks: {
+          async onRoute(ctx) {
+            ctx.body = 'hello';
+          },
+        },
+      }],
+    });
+
+    await server.start();
+
+    await request(`http://localhost:${TEST_PORT}`)
+      .get('/')
+      .expect(200)
+      .expect('hello');
+
+    return server.close();
+  });
+  it('svrx.events', async () => {
+    const spy = sinon.spy();
+
+    const server = svrx({
+      open: false,
+      livereload: false,
+    });
+
+    server.on('hello', spy);
+    await server.emit('hello', 'world');
+
+    expect(spy.calledOnce).to.equal(true);
+    server.off('hello', spy);
+    await server.emit('hello', 'world');
+
+    expect(spy.calledOnce).to.equal(true);
+  });
+
+  it('svrx.reload()', async () => {
+    const spy = sinon.spy();
+
+    const server = svrx({
+      open: false,
+      livereload: false,
+    });
+
+    server.on('file:change', spy);
+
+    await server.reload();
+
+    expect(spy.calledOnce).to.equal(true);
   });
 });
