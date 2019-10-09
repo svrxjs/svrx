39f65c7317055413d1002983fc07ef373b74ef34
C.Y.Xu <itsxcy@gmail.com>
2019-10-09 21:08:50 +0800

    refactor(plugin): refactor plugin package name parse (#106)
    * refactor(plugin): refactor plugin package name parse
    
    * build(codecov): update patch diff cov to 100%
    
    * build(codecov): update threshold to 0.01%
    
    * test(plugin): addup tests
    
    * fix(helper): fix scoped plugin name regex
    
    * fix(helper): use startsWith instead of indexOf
    
    * test(config): add test for inline config

--- packages/svrx/__tests__/spec/configure/inline.js
+++ packages/svrx/__tests__/spec/configure/inline.js
@@ -1,8 +1,12 @@
 const expect = require('expect.js');
 const libPath = require('path');
+const fs = require('fs');
+const sinon = require('sinon');
 const { createServer } = require('../../util');
+require('../../../lib/configure/index'); // required for module get
 
-const TEST_PLUGIN_PATH = libPath.join(__dirname, '../fixture/plugin/svrx-plugin-test');
+const TEST_PLUGIN_PATH = libPath.join(__dirname, '../../fixture/plugin/svrx-plugin-test');
+const NO_PACKAGE_PLUGIN_PATH = libPath.join(__dirname, '../../fixture/plugin/svrx-plugin-no-package');
 
 describe('Inline/RC File Config', () => {
   describe('plugins', () => {
@@ -30,16 +34,58 @@ describe('Inline/RC File Config', () => {
       expect(testPlugin).not.to.be(undefined);
     });
 
-    it('should parse local plugin name correctly', () => {
-      const server = createServer({
-        plugins: [
-          {
-            path: TEST_PLUGIN_PATH,
-          },
-        ],
+    // pass plugin by path
+    describe('Local plugin name parse', () => {
+      it('should parse normal plugin name correctly', () => {
+        const server = createServer({
+          plugins: [
+            {
+              path: TEST_PLUGIN_PATH,
+            },
+          ],
+        });
+        const testPlugin = server.config.getPlugin('test');
+        expect(testPlugin).not.to.be(undefined);
       });
-      const testPlugin = server.config.getPlugin('test');
-      expect(testPlugin).not.to.be(undefined);
+      it('should get plugin name without package.json correctly', () => {
+        const server = createServer({
+          plugins: [
+            {
+              path: NO_PACKAGE_PLUGIN_PATH,
+            },
+          ],
+        });
+        const testPlugin = server.config.getPlugin('no-package');
+        expect(testPlugin).not.to.be(undefined);
+      });
+      it('should parse plugin name from path correctly', () => {
+        const fakes = [
+          { path: '/fake/path/for/normal/plugin', name: 'svrx-plugin-foo', pluginName: 'foo' },
+          { path: '/fake/path/for/normal/dash/plugin', name: 'svrx-plugin-foo-bar', pluginName: 'foo-bar' },
+          { path: '/fake/path/for/scope/plugin', name: '@scope/svrx-plugin-foo', pluginName: '@scope/foo' },
+          { path: '/fake/path/for/scope/dash/plugin', name: '@scope/svrx-plugin-foo-bar', pluginName: '@scope/foo-bar' },
+        ];
+        const moduleId = '/svrx/packages/svrx/lib/configure/index.js';
+
+        fakes.forEach((fake) => {
+          const fakePackagePath = `${fake.path}/package.json`;
+          const existsSyncStub = sinon.stub(fs, 'existsSync');
+          const configModule = module.children.find((mod) => mod.id.endsWith(moduleId));
+          const requireStub = sinon.stub(configModule, 'require');
+
+          // fake functions
+          requireStub.withArgs(fakePackagePath).callsFake(() => ({ name: fake.name }));
+          existsSyncStub.withArgs(fakePackagePath).callsFake(() => true);
+
+          const server = createServer({
+            plugins: [{ path: fake.path }],
+          });
+          const testPlugin = server.config.getPlugin(fake.pluginName);
+          expect(testPlugin).not.to.be(undefined);
+          requireStub.restore();
+          existsSyncStub.restore();
+        });
+      });
     });
   });
 });
--- packages/svrx/lib/util/helper.js
+++ packages/svrx/lib/util/helper.js
@@ -5,12 +5,12 @@ const libPath = require('path');
 const libFs = require('fs');
 const _ = require('lodash');
 const os = require('os');
-
 const CONST = require('../constant');
 
+const { PLUGIN_PREFIX } = CONST;
+const scopeAndNameRegex = /^@([a-z\d][\w-.]+)\/([a-z\d][\w-.]*)/;
 const o2str = {}.toString;
 const { slice } = [];
-
 
 // return promise by callback node-callback-style handler
 function npCall(callback, args, ctx) {
@@ -26,20 +26,50 @@ function npCall(callback, args, ctx) {
   });
 }
 
-
+/**
+ * combine pluginName to packageName
+ * - foo -> svrx-plugin-foo
+ * - foo-bar -> svrx-plugin-foo-bar
+ * - @scope/foo -> @scope/svrx-plugin-foo
+ * - @scope/foo-bar -> @scope/svrx-plugin-foo-bar
+ * @param name
+ * @returns {string|*}
+ */
 function normalizePluginName(name) {
-  const combineName = (n) => (n.indexOf(CONST.PLUGIN_PREFIX) !== 0 ? CONST.PLUGIN_PREFIX + n : n);
-  const isScoped = name.indexOf('/') >= 0;
+  const combineName = (n) => (n.startsWith(PLUGIN_PREFIX) ? n : PLUGIN_PREFIX + n);
+  const isScoped = name.startsWith('@');
 
   if (isScoped) {
-    const matches = /^@([a-zA-Z0-9_-]+)\/(.*)$/.exec(name);
-    if (matches.length === 3) {
+    const matches = scopeAndNameRegex.exec(name);
+    if (matches) {
       const scope = matches[1];
       const realName = matches[2];
       return `@${scope}/${combineName(realName)}`;
     }
+    return null;
   }
   return combineName(name);
+}
+
+/**
+ * parse packageName to pluginName (revert normalizePluginName())
+ * @param packageName
+ */
+function parsePluginName(packageName) {
+  const isScoped = packageName.startsWith('@');
+  const removePrefix = (n) => (n.startsWith(PLUGIN_PREFIX) ? n.slice(PLUGIN_PREFIX.length) : null);
+
+  if (isScoped) {
+    const matches = scopeAndNameRegex.exec(packageName);
+    if (matches) {
+      const scope = matches[1];
+      const realName = matches[2];
+      const formattedName = removePrefix(realName);
+      return formattedName ? `@${scope}/${formattedName}` : null;
+    }
+    return null;
+  }
+  return removePrefix(packageName);
 }
 
 function isReadableStream(test) {
@@ -165,6 +195,7 @@ function isFn(o) {
 
 
 exports.normalizePluginName = normalizePluginName;
+exports.parsePluginName = parsePluginName;
 exports.getExternalIp = _.memoize(getExternalIp);
 exports.isReadableStream = isReadableStream;
 exports.getCert = _.memoize(getCert);
