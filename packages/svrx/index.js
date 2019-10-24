cf4552b396f8afb3a505935293440eace72d6a0c
xuchaoying <itsxcy@gmail.com>
2019-10-24 16:03:12 +0800

    test: ignore win32 test

--- packages/svrx/index.js
+++ packages/svrx/index.js
@@ -34,12 +34,12 @@ function create(options) {
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+/* istanbul ignore if */
 if (process.platform === 'win32') {
   const rl = require('readline').createInterface({ // eslint-disable-line
     input: process.stdin,
     output: process.stdout,
   });
-
   rl.on('SIGINT', () => {
     process.emit('SIGINT');
   });
