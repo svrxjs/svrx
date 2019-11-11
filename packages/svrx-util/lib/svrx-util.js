f6daab084b17e7d1c461d2621a08722d07c81059
xuchaoying <itsxcy@gmail.com>
2019-11-11 15:36:48 +0800

    Revert "fix: replace global-npm.install with npminstall (#148)"
    This reverts commit cb1f7910

--- packages/svrx-util/lib/svrx-util.js
+++ packages/svrx-util/lib/svrx-util.js
@@ -1,5 +1,4 @@
 exports.rcFileRead = require('./rc-file-read');
-exports.readJSON = require('./readJSON');
 exports.logger = require('./logger');
 exports.npm = require('./npm');
 exports.c2k = require('./c2k');
