d2370f3a471fdfb186eb3259075efe804484fe33
xuchaoying <itsxcy@gmail.com>
2019-11-18 10:34:03 +0800

    refactor: remove core package manager

--- packages/svrx-util/lib/package-manager/core-package-manager.js
+++
@@ -1,9 +0,0 @@
-const PackageManager = require('./package-manager');
-
-class PluginPackageManager extends PackageManager {
-  // constructor(options) {
-  //   super(options);
-  // }
-}
-
-module.exports = PluginPackageManager;
--- packages/svrx-util/lib/package-manager/index.js
+++ packages/svrx-util/lib/package-manager/index.js
@@ -1,12 +1,12 @@
 const PluginPackageManager = require('./plugin-package-manager');
-const CorePackageManager = require('./core-package-manager');
+const PackageManager = require('./package-manager');
 
 const PackageManagerCreator = ({ plugin, ...options }) => (plugin
   ? new PluginPackageManager({
     name: plugin, // plugin name: foo, foo-bar, @scope/foo
     ...options,
   })
-  : new CorePackageManager({
+  : new PackageManager({
     name: 'svrx',
     ...options,
   })
