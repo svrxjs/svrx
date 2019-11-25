3dcaefae0aa5225e8ac1462980e2525494ed6f45
xuchaoying <itsxcy@gmail.com>
2019-11-25 11:59:47 +0800

    build(temp): add todo to config

--- packages/svrx/lib/configure/index.js
+++ packages/svrx/lib/configure/index.js
@@ -98,6 +98,22 @@ class Configure {
     return this[BUILTIN_OPTION].watch(pathes, callback);
   }
 
+  // for ui
+  builtinsSet(settings = {}) {
+    Object.keys(settings).forEach((key) => {
+      this.set(key, settings[key]);
+    });
+    // todo 部分 内置项 热重启
+  }
+
+  // for ui
+  pluginsSet(settings = {}) { // eslint-disable-line
+    // todo 发送事件，交给插件自行处理
+    // todo 提供插件热重启方法
+    console.log('pluginsSet');
+    console.log(settings); // { qrcode: { cli: false, ui: true, console: false } }
+  }
+
   /**
    * get all plugins
    * @returns {Array}
