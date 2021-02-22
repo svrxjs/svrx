c6717274bf1f764383c3a598e1d1af5af89e39f5
C.Y.XU <itsxcy@gmail.com>
2021-02-22 20:37:04 +0800

    fix(util): use child_process to exec npm commands directly
    avoid use npm api: #191

--- packages/svrx-util/lib/package-manager/index.js
+++ packages/svrx-util/lib/package-manager/index.js
@@ -1,7 +1,21 @@
 const PluginPackageManager = require('./plugin-package-manager');
 const PackageManager = require('./package-manager');
 
-const PackageManagerCreator = ({ plugin, ...options } = {}) => (plugin
+/**
+ * @example
+ const pm = PackageManagerCreator({
+  plugin: 'lodash', // will install svrx core if missing
+  path: undefined, // if install from path
+  version: '1.0.0',
+  coreVersion: '1.0.1', // version of current svrx core
+  autoClean: true, // auto remove old packages
+});
+ const pkg = await pm.load();
+ */
+const PackageManagerCreator = ({
+  plugin,
+  ...options
+} = {}) => (plugin
   ? new PluginPackageManager({
     name: plugin, // plugin name: foo, foo-bar, @scope/foo
     ...options,
--- packages/svrx-util/lib/svrx-util.js
+++ packages/svrx-util/lib/svrx-util.js
@@ -2,5 +2,4 @@ exports.PackageManagerCreator = require('./package-manager');
 exports.nameFormatter = require('./name-formatter');
 exports.rcFileRead = require('./rc-file-read');
 exports.logger = require('./logger');
-exports.npm = require('./npm');
 exports.c2k = require('./c2k');
